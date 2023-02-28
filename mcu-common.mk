b23cfd2f8b4db4dbc720feaecb9859e20a2e92cf
wallacegibbon <wallacegibbon@aliyun.com>
2023-02-28 22:46:53 +0800

    The makefile (mcu-common.mk) that is useful for MCUs (CH32V, STM32, ...) is tested.

---
+++ mcu-common.mk
@@ -0,0 +1,75 @@
+## Variables you have to define before including this makefile:
+## CROSS_COMPILER_PREFIX, OPENOCD, OPENOCD_ARGS, TARGET, BUILD_DIR, ARCH,
+## CROSS_C_SOURCE_FILES, CROSS_ASM_SOURCE_FILES, CROSS_C_ASM_INCLUDES
+##
+## Variables you can define if you need:
+## CUSTOM_C_ASM_FLAGS
+
+CROSS_OBJECTS += $(addprefix $(BUILD_DIR)/, $(notdir $(CROSS_C_SOURCE_FILES:.c=.c.o)))
+CROSS_OBJECTS += $(addprefix $(BUILD_DIR)/, $(notdir $(CROSS_ASM_SOURCE_FILES:.S=.S.o)))
+
+CROSS_C_ASM_FLAGS = $(ARCH) -W -g -Os -ffunction-sections -fdata-sections \
+-fno-common -fno-builtin $(CROSS_C_ASM_INCLUDES) $(CUSTOM_C_ASM_FLAGS) \
+
+CROSS_LD_FLAGS = $(ARCH) -T./link.ld -nostartfiles \
+-specs=nosys.specs -specs=nano.specs \
+-Wl,--gc-sections -Wl,--no-relax -Wl,-Map=$(BUILD_DIR)/$(TARGET).map,--cref \
+
+CROSS_CC = "$(CROSS_COMPILER_PREFIX)gcc"
+CROSS_LD = "$(CROSS_COMPILER_PREFIX)gcc"
+CROSS_GDB = "$(CROSS_COMPILER_PREFIX)gdb"
+CROSS_OBJCOPY = "$(CROSS_COMPILER_PREFIX)objcopy"
+CROSS_OBJDUMP = "$(CROSS_COMPILER_PREFIX)objdump"
+CROSS_SIZE = "$(CROSS_COMPILER_PREFIX)size"
+
+vpath %.c $(sort $(dir $(CROSS_C_SOURCE_FILES)))
+vpath %.S $(sort $(dir $(CROSS_ASM_SOURCE_FILES)))
+
+.PHONY: all flash openocd debug clean
+
+all: $(BUILD_DIR)/$(TARGET).hex $(BUILD_DIR)/$(TARGET).bin
+
+$(BUILD_DIR)/%.c.o: %.c | $(BUILD_DIR)
+	@echo "\tCC $< ..."
+	@$(CROSS_CC) $(CROSS_C_ASM_FLAGS) -MMD -MF"$(@:%.o=%.d)" -c -o $@ $<
+
+$(BUILD_DIR)/%.S.o: %.S | $(BUILD_DIR)
+	@echo "\tAS $< ..."
+	@$(CROSS_CC) $(CROSS_C_ASM_FLAGS) -MMD -MF"$(@:%.o=%.d)" -c -o $@ $<
+
+$(BUILD_DIR)/$(TARGET).elf: $(CROSS_OBJECTS)
+	@echo "\tLD $@ ..."
+	@$(CROSS_LD) $(CROSS_LD_FLAGS) -o $@ $^
+	@$(CROSS_OBJDUMP) -S -D $@ > $@.lss
+	@echo
+	@echo "\tMemory Usage:"
+	@$(CROSS_SIZE) $@
+	@echo
+
+$(BUILD_DIR)/$(TARGET).hex: $(BUILD_DIR)/$(TARGET).elf
+	@echo "\tGenerating HEX file ..."
+	@$(CROSS_OBJCOPY) -Oihex $< $@
+
+$(BUILD_DIR)/$(TARGET).bin: $(BUILD_DIR)/$(TARGET).elf
+	@echo "\tGenerating BIN file ..."
+	@$(CROSS_OBJCOPY) -Obinary $< $@
+
+$(BUILD_DIR):
+	@mkdir $@
+
+flash:
+	@$(OPENOCD) $(OPENOCD_ARGS) \
+		-c "program $(BUILD_DIR)/$(TARGET).hex verify reset exit"
+
+openocd:
+	@$(OPENOCD) $(OPENOCD_ARGS)
+
+debug:
+	@$(CROSS_GDB) $(BUILD_DIR)/$(TARGET).elf \
+		--eval-command="target extended-remote localhost:3333"
+
+clean:
+	@rm -rf $(BUILD_DIR)
+
+-include $(wildcard $(BUILD_DIR)/*.d)
+
