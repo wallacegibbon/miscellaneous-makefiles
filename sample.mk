3101adb337a039027be5aec1c4a9a338df37d9d6
wallacegibbon <wallacegibbon@aliyun.com>
2023-02-28 22:50:54 +0800

    Improvement: A sample makefile "sample.mk" showing how to use this project is added.

---
+++ sample.mk
@@ -0,0 +1,26 @@
+CROSS_COMPILER_PREFIX = $(HOME)/MRS_Toolchain_Linux_x64_V1.60/RISC-V Embedded GCC/bin/riscv-none-embed-
+OPENOCD_PATH = $(HOME)/MRS_Toolchain_Linux_x64_V1.60/OpenOCD
+OPENOCD = "$(OPENOCD_PATH)/bin/openocd"
+OPENOCD_ARGS = -f "$(OPENOCD_PATH)/bin/wch-riscv.cfg"
+
+## To change target name: `TARGET=xxx make`(unix only) or `make TARGET=xxx`
+TARGET = target
+BUILD_DIR = build
+
+LIB_PERIPHERAL_DIR = $(HOME)/CH32_standard_peripheral_library/CH32V103
+ARCH = -march=rv32imac -mabi=ilp32
+#ARCH = -march=rv32ec -mabi=ilp32e
+
+CROSS_C_SOURCE_FILES += $(wildcard $(LIB_PERIPHERAL_DIR)/src/*.c)
+CROSS_C_SOURCE_FILES += $(wildcard ./src/screen-library-mcu/*.c)
+CROSS_C_SOURCE_FILES += $(wildcard ./src/screen-library-mcu/ch32v103/*.c)
+CROSS_C_SOURCE_FILES += $(wildcard ./src/*.c)
+
+CROSS_ASM_SOURCE_FILES = $(wildcard ./*.S)
+
+CROSS_C_ASM_INCLUDES = \
+-I$(LIB_PERIPHERAL_DIR)/inc \
+-I./src/screen-library-mcu/ch32v103 -I./src/screen-library-mcu -I./src \
+
+include ../miscellaneous-makefiles/mcu-common.mk
+
