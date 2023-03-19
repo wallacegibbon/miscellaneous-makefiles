6433d272013b47922e7cde07b0a357ce17390290
wallacegibbon <wallacegibbon@aliyun.com>
2023-03-19 15:42:54 +0800

    Improvements: The sample for `cross-gcc-mcu` is updated to use ch32v20x by default.

--- cross-gcc-mcu-sample.mk
+++ cross-gcc-mcu-sample.mk
@@ -5,12 +5,12 @@ OPENOCD_ARGS = -f interface/wlink.cfg -f target/wch-riscv.cfg
 ARCH = -march=rv32imac -mabi=ilp32
 #ARCH = -march=rv32ec -mabi=ilp32e
 
-CH32_STD_LIB_DIR = $(HOME)/playground/ch32-standard-library/ch32v10x
+CH32_STD_LIB_DIR = $(HOME)/playground/ch32-standard-library/ch32v20x
 
 CROSS_C_SOURCE_FILES += $(wildcard $(CH32_STD_LIB_DIR)/peripheral/src/*.c)
 CROSS_C_SOURCE_FILES += $(wildcard $(CH32_STD_LIB_DIR)/core/*.c)
 CROSS_C_SOURCE_FILES += $(wildcard ./screen-library-mcu/*.c)
-CROSS_C_SOURCE_FILES += $(wildcard ./screen-library-mcu/ch32v10x/*.c)
+CROSS_C_SOURCE_FILES += $(wildcard ./screen-library-mcu/ch32v/*.c)
 CROSS_C_SOURCE_FILES += $(wildcard ./src/*.c)
 
 CROSS_ASM_SOURCE_FILES += $(CH32_STD_LIB_DIR)/sample/startup.S
@@ -19,10 +19,10 @@ CROSS_LINKER_SCRIPT = $(CH32_STD_LIB_DIR)/sample/default.ld
 
 CROSS_C_INCLUDES = \
 $(CH32_STD_LIB_DIR)/peripheral/inc $(CH32_STD_LIB_DIR)/core \
-./screen-library-mcu/ch32v10x ./screen-library-mcu ./src \
+./screen-library-mcu/ch32v ./screen-library-mcu ./src \
 
 OPENOCD_FLASH_COMMANDS = \
--c "program $<" -c wlink_reset_resume -c exit
+-c "program $< verify" -c wlink_reset_resume -c exit
 
 include ../miscellaneous-makefiles/cross-gcc-mcu.mk
 
