899b5f3e8779c2bc3784833f2773f722c3f80406
wallacegibbon <wallacegibbon@aliyun.com>
2023-12-18 23:56:01 +0800

    The emcc makefile got introduced. (but not fully tested yet)

---
+++ simple-emcc-single-sample.mk
@@ -0,0 +1,13 @@
+C_SOURCE_FILES += $(wildcard ./screen-library-mcu/*.c)
+C_SOURCE_FILES += $(wildcard ./screen-library-mcu/sdlv1/*.c)
+C_SOURCE_FILES += $(wildcard ./src/*.c)
+
+C_INCLUDES += ./src ./screen-library-mcu ./screen-library-mcu/sdlv1
+
+TARGET = serialport-demo-sdlv1
+
+C_FLAGS += $(shell sdl-config --cflags)
+LD_FLAGS += $(shell sdl-config --libs)
+
+include ../miscellaneous-makefiles/simple-emcc-single.mk
+
---
+++ simple-emcc-single.mk
@@ -0,0 +1,30 @@
+OBJECTS += $(addprefix $(BUILD_DIR)/, $(notdir $(C_SOURCE_FILES:.c=.c.o)))
+
+CC = emcc
+
+BUILD_DIR ?= build
+TARGET ?= target
+
+C_FLAGS += $(addprefix -I, $(C_INCLUDES)) -MMD -MP -MF"$(@:%.o=%.d)"
+LD_FLAGS +=
+
+vpath %.c $(sort $(dir $(C_SOURCE_FILES)))
+
+.PHONY: all clean
+
+all: $(BUILD_DIR)/$(TARGET)
+
+$(BUILD_DIR)/%.c.o: %.c | $(BUILD_DIR)
+	$(CC) -o $@ -c $< $(C_FLAGS)
+
+$(BUILD_DIR)/$(TARGET): $(OBJECTS)
+	$(CC) -o $@ $^ $(LD_FLAGS)
+
+$(BUILD_DIR):
+	mkdir $@
+
+clean:
+	rm -rf build
+
+-include $(OBJECTS:.o=.d)
+
