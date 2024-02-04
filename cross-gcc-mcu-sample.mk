61992001b658f52c904d82160a9b349c1930bf69
wallacegibbon <wallacegibbon@outlook.com>
2024-02-05 00:24:07 +0800

    Trivial changes: The openocd command and configuration lines are in pairs now.

--- cross-gcc-mcu-sample.mk
+++ cross-gcc-mcu-sample.mk
@@ -1,10 +1,11 @@
 CROSS_COMPILER_PREFIX = C:/MounRiver/MounRiver_Studio/toolchain/RISC-V Embedded GCC/bin/riscv-none-embed-
 #CROSS_COMPILER_PREFIX = $(HOME)/MRS_Toolchain_Linux_x64_V1.80/RISC-V Embedded GCC/bin/riscv-none-embed-
+
 OPENOCD = C:/MounRiver/MounRiver_Studio/toolchain/OpenOCD/bin/openocd.exe
-#OPENOCD = $(HOME)/MRS_Toolchain_Linux_x64_V1.80/OpenOCD/bin/openocd
-#OPENOCD = /usr/local/bin/openocd
 OPENOCD_ARGS = -f C:/MounRiver/MounRiver_Studio/toolchain/OpenOCD/bin/wch-riscv.cfg
+#OPENOCD = $(HOME)/MRS_Toolchain_Linux_x64_V1.80/OpenOCD/bin/openocd
 #OPENOCD_ARGS = -f $(HOME)/MRS_Toolchain_Linux_x64_V1.80/OpenOCD/bin/wch-riscv.cfg
+#OPENOCD = /usr/local/bin/openocd
 #OPENOCD_ARGS = -f interface/wlink.cfg -f target/wch-riscv.cfg
 
 ARCH = -march=rv32imafc -mabi=ilp32f
