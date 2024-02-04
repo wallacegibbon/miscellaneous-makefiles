23f1b2347aa880d6c99093233efcca26a4e2cac0
wallacegibbon <wallacegibbon@outlook.com>
2024-02-05 00:18:28 +0800

    Feature: The `cross-gcc-mcu-sample.mk` has been upgraded to support Windows.

--- cross-gcc-mcu-sample.mk
+++ cross-gcc-mcu-sample.mk
@@ -1,19 +1,21 @@
-CROSS_COMPILER_PREFIX = $(HOME)/MRS_Toolchain_Linux_x64_V1.80/RISC-V Embedded GCC/bin/riscv-none-embed-
-OPENOCD = $(HOME)/MRS_Toolchain_Linux_x64_V1.80/OpenOCD/bin/openocd
-OPENOCD_ARGS = -f $(HOME)/MRS_Toolchain_Linux_x64_V1.80/OpenOCD/bin/wch-riscv.cfg
+CROSS_COMPILER_PREFIX = C:/MounRiver/MounRiver_Studio/toolchain/RISC-V Embedded GCC/bin/riscv-none-embed-
+#CROSS_COMPILER_PREFIX = $(HOME)/MRS_Toolchain_Linux_x64_V1.80/RISC-V Embedded GCC/bin/riscv-none-embed-
+OPENOCD = C:/MounRiver/MounRiver_Studio/toolchain/OpenOCD/bin/openocd.exe
+#OPENOCD = $(HOME)/MRS_Toolchain_Linux_x64_V1.80/OpenOCD/bin/openocd
 #OPENOCD = /usr/local/bin/openocd
+OPENOCD_ARGS = -f C:/MounRiver/MounRiver_Studio/toolchain/OpenOCD/bin/wch-riscv.cfg
+#OPENOCD_ARGS = -f $(HOME)/MRS_Toolchain_Linux_x64_V1.80/OpenOCD/bin/wch-riscv.cfg
 #OPENOCD_ARGS = -f interface/wlink.cfg -f target/wch-riscv.cfg
 
-#ARCH = -march=rv32imafc -mabi=ilp32f
-ARCH = -march=rv32imac -mabi=ilp32
+ARCH = -march=rv32imafc -mabi=ilp32f
+#ARCH = -march=rv32imac -mabi=ilp32
 #ARCH = -march=rv32ec -mabi=ilp32e
 
-CH32_STD_LIB_DIR = $(HOME)/playground/ch32-standard-library/ch32v30x
+CH32_STD_LIB_DIR = ../ch32-standard-library/ch32v30x
+#CH32_STD_LIB_DIR = $(HOME)/playground/ch32-standard-library/ch32v30x
 
 CROSS_C_SOURCE_FILES += $(wildcard $(CH32_STD_LIB_DIR)/peripheral/src/*.c)
 CROSS_C_SOURCE_FILES += $(wildcard $(CH32_STD_LIB_DIR)/core/*.c)
-CROSS_C_SOURCE_FILES += $(wildcard ./screen-library-mcu/*.c)
-CROSS_C_SOURCE_FILES += $(wildcard ./screen-library-mcu/ch32v/*.c)
 CROSS_C_SOURCE_FILES += $(wildcard ./src/*.c)
 
 CROSS_ASM_SOURCE_FILES += $(CH32_STD_LIB_DIR)/sample/startup.S
