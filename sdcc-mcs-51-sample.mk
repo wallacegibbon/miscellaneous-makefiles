a1157bb4347896b686f8319a3d077826f4ce81c8
wallacegibbon <wallacegibbon@outlook.com>
2024-04-26 00:29:36 +0800

    Trivial improvement: The `sdcc-mcs-51-sample.mk` got updated. (An assembly file example got added)

--- sdcc-mcs-51-sample.mk
+++ sdcc-mcs-51-sample.mk
@@ -2,7 +2,7 @@ CROSS_COMPILER_PREFIX = C:/Program Files/SDCC/bin/
 
 CROSS_C_SOURCE_FILES += $(wildcard ./src/*.c)
 
-CROSS_ASM_SOURCE_FILES +=
+CROSS_ASM_SOURCE_FILES += $(wildcard ./src/*.asm)
 
 CROSS_C_FLAGS += --model-small --xram-size 0x0100 --xram-loc 0x0100 --code-size 0x2800
 
